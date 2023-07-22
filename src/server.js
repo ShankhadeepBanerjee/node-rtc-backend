80794361585355b28e72269baf9f289b46aaa641
Shankhadeep <shankhadeep17.sb@gmail.com>
2023-07-22 11:15:29 +0530

    feat: adds 'turn-creds' endpoint for getting Turn server creds from 'Metered'

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const axios = require('axios');
+require('dotenv').config();
 const { Server } = require('socket.io');
 const { meetRoomHandler } = require('./lib/utils/meetRoomHandler');
 const cors = require('cors');
+
 const app = express();
+
 app.use(express.json());
 app.use(cors({allowedHeaders: '*'}));
+
 const httpServer = require('http').createServer(app);
 const io = new Server(httpServer, {
     cors: {
@@ -26,6 +31,17 @@ io.on('connection', (socket) => {
 
 app.get('/', (req, res) => {
   res.json({message: 'Hello World!'});
+})
+
+app.get('/turn-creds', async (req, res) => {
+  const URL = `${process.env.METERED_BASE_URL}/api/v1/turn/credentials?apiKey=${process.env.METERED_API_KEY}`;
+  try {
+		const response = await axios.get(URL);
+		res.status(200).json(response.data);
+	} catch (err) {
+		res.status(500).json({ message: err });
+	}
+  
 })
 
 const port = process.env.PORT || 4000;
